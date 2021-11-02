c367ae6b5eaf217e2133b3f20f0cb08af0d681ee
hyehyeonmoon <angelfox4@naver.com>
2021-11-02 16:42:06 +0900

    Feat: #styled_env - styled-component global과 theme 설정
    typescript 용 styled component 설치
    용도에 맞는 파일이름 변경
    theme과 global 설정 및 export

---
+++ client/src/styledComponents/globalStyle.ts
@@ -0,0 +1,16 @@
+import { createGlobalStyle } from 'styled-components';
+import { normalize } from 'styled-normalize';
+
+const GlobalStyle = createGlobalStyle`
+  ${normalize}
+  
+  *, *::before, *::after {
+    box-sizing: border-box;
+  }
+
+  body {
+    font-family: "Noto Sans KR";
+  }
+`;
+
+export default GlobalStyle;
