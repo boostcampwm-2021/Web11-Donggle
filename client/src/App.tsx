0c580a6c7d672cb104e86e7bef8e3af53799e5bb
isanghaessi <jesse1231@naver.com>
2021-11-02 20:46:34 +0900

    fix: GlobalStyle 적용
    - App.tsx에 GlobalStyle 적용

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,17 +2,21 @@ import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import { NotFoundPage, MainPage } from '@pages/index';
 import { GlobalStore } from '@stores/index';
+import { GlobalStyle } from '@styledComponents/index';
 
 const App: React.FC = () => {
   return (
-    <GlobalStore>
-      <BrowserRouter>
-        <Switch>
-          <Route exact path="/" component={MainPage} />
-          <Route component={NotFoundPage} />
-        </Switch>
-      </BrowserRouter>
-    </GlobalStore>
+    <>
+      <GlobalStyle />
+      <GlobalStore>
+        <BrowserRouter>
+          <Switch>
+            <Route exact path="/" component={MainPage} />
+            <Route component={NotFoundPage} />
+          </Switch>
+        </BrowserRouter>
+      </GlobalStore>
+    </>
   );
 };
 
