8dc8c3318be019d833b1e0bad5611019b2d42791
hyehyeonmoon <angelfox4@naver.com>
2021-11-02 21:37:38 +0900

    Feat: #2 - 모달 레이아웃 스타일 조정
    배경색을 string으로 줘서 적용되지 않는 오류 해결
    내용에 따라 모달창의 크기가 변하도록 설정

--- client/src/components/modal/index.style.tsx
+++ client/src/components/modal/index.style.tsx
@@ -1,7 +1,22 @@
 import styled from 'styled-components';
 
+export const ModalOverlay = styled.div`
+  position: fixed;
+  top: 0;
+  left: 0;
+  bottom: 0;
+  right: 0;
+  background-color: rgba(0, 0, 0, 0.6);
+
+  z-index: 99;
+`;
+
 export const ModalWrapper = styled.div`
   position: absolute;
+  max-width: 80%;
+  width: auto;
+  max-height: 80%;
+  height: auto;
   top: 50%;
   left: 50%;
   transform: translate(-60%, -50%);
@@ -14,5 +29,7 @@ export const ModalWrapper = styled.div`
   box-shadow: 0px 4px 10px rgba(51, 51, 51, 0.1),
     0px 0px 4px rgba(51, 51, 51, 0.05);
   border-radius: 10px;
-  background-color: rgba(0, 0, 0, 0.4);
+  background-color: #ffffff;
+
+  z-index: 100;
 `;
