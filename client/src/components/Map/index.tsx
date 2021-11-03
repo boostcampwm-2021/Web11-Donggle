7a5e0c429b08fd148fb07531012417df5e6f632b
jwhong <hongjw1938@gmail.com>
2021-11-03 17:26:45 +0900

    Feat: #20 메인페이지 Map 렌더링하기
    geoLocation API 기반 Kakao Map 렌더링 완료

--- client/src/components/Map/index.tsx
+++ client/src/components/Map/index.tsx
@@ -5,14 +5,36 @@ declare global {
   interface Window {
     kakao: any;
   }
+
+  interface latlng {
+    latitude: number;
+    longitude: number;
+  }
 }
 
 const Map: React.FC = () => {
   const mapWrapper = useRef<HTMLDivElement>(null);
-  const [{ latitude, longitude }, setCoord] = useState({
+  const map = useRef(null);
+  const [{ latitude, longitude }, setLatlng] = useState({
     latitude: 33.450701,
     longitude: 126.570667,
   });
+
+  const getLocation = (map) => {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition((position) => {
+        const { latitude, longitude } = position.coords;
+        map.setCenter({ latitude, longitude });
+      });
+    } else {
+      map.setCenter({ latitude: 33.450701, longitude: 126.570667 });
+    }
+  };
+
+  const changeCenter = (map) => () => {
+    const latlng = map.getCenter();
+    setLatlng({ latitude: latlng.getLat(), longitude: latlng.getLng() });
+  };
 
   useEffect(() => {
     const options = {
@@ -20,9 +42,21 @@ const Map: React.FC = () => {
       level: 3,
     };
 
-    new window.kakao.maps.Map(mapWrapper.current, options);
+    map.current = new window.kakao.maps.Map(mapWrapper.current, options);
+    getLocation(map.current);
+
+    window.kakao.maps.event.addListener(map.current, 'center_changed', () =>
+      changeCenter(map.current),
+    );
+    return () =>
+      window.kakao.maps.event.removeListener(
+        map.current,
+        'center_changed',
+        changeCenter(map.current),
+      );
   }, []);
 
   return <MapWrapper ref={mapWrapper} />;
 };
+
 export default Map;
