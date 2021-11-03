87ae65fe9d93e55d2e4467148d60c5f4aed62287
jwhong <hongjw1938@gmail.com>
2021-11-03 18:00:26 +0900

    Feat: #20 메인페이지 Map 레이아웃 위치 정보 반영
    현재 위치 정보 반영하여 지도 보여주기 구현

--- client/src/components/Map/index.tsx
+++ client/src/components/Map/index.tsx
@@ -20,17 +20,21 @@ const Map: React.FC = () => {
     longitude: 126.570667,
   });
 
+  // 초기 위치 값 가져오기
   const getLocation = (map) => {
     if (navigator.geolocation) {
       navigator.geolocation.getCurrentPosition((position) => {
         const { latitude, longitude } = position.coords;
-        map.setCenter({ latitude, longitude });
+        const coord = new window.kakao.maps.LatLng(latitude, longitude);
+        map.setCenter(coord);
       });
     } else {
-      map.setCenter({ latitude: 33.450701, longitude: 126.570667 });
+      const coord = new window.kakao.maps.LatLng(33.450701, 126.570667);
+      map.setCenter(coord);
     }
   };
 
+  // 중심 Position 변경 시 동작하는 handler
   const changeCenter = (map) => () => {
     const latlng = map.getCenter();
     setLatlng({ latitude: latlng.getLat(), longitude: latlng.getLng() });
@@ -45,13 +49,15 @@ const Map: React.FC = () => {
     map.current = new window.kakao.maps.Map(mapWrapper.current, options);
     getLocation(map.current);
 
-    window.kakao.maps.event.addListener(map.current, 'center_changed', () =>
+    window.kakao.maps.event.addListener(
+      map.current,
+      'dragend',
       changeCenter(map.current),
     );
     return () =>
       window.kakao.maps.event.removeListener(
         map.current,
-        'center_changed',
+        'dragend',
         changeCenter(map.current),
       );
   }, []);
