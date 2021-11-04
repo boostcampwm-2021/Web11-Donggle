53fa158f8c75638e19698f2a9afb38322f856857
jwhong <hongjw1938@gmail.com>
2021-11-05 06:16:31 +0900

    Fix: #29 Server 측 Query 스트링 변경 및 정규 표현식 기반 DB 쿼리문 개선

--- server/src/api/map.ts
+++ server/src/api/map.ts
@@ -1,11 +1,16 @@
 import express, { Request, Response } from 'express';
-import { parseCode, queryPolygon } from '@services/map';
+import { queryPolygon } from '@services/map';
 
 const router: express.Router = express.Router();
 
 router.get('/polygon', async (req: Request, res: Response) => {
-  const { scale, code } = req.query;
-  const polygon = await queryPolygon(Number(scale), parseCode(code as string));
+  const { scale, big, medium, small } = req.query;
+  const polygon = await queryPolygon(
+    Number(scale),
+    big as string,
+    medium as string,
+    small as string,
+  );
   res.json(polygon);
 });
 
