d4c09138b5a6c7414de87c08f73783751aa7f7cc
jwhong <hongjw1938@gmail.com>
2021-11-04 15:08:08 +0900

    Feat: #21 Map 조작
    통계청 API 행정경계 종로구 폴리곤 생성 코드 구현
    UTM-K -> WGS84 변환을 위한 proj4 라이브러리 설치 및 구현
    kakao map type 라이브러리 설치 및 리팩토링

--- client/src/components/Map/index.tsx
+++ client/src/components/Map/index.tsx
@@ -1,66 +1,99 @@
 import React, { useRef, useEffect, useState } from 'react';
 import MapWrapper from '@components/Map/index.style';
+import transCoord from './data';
 
-declare global {
-  interface Window {
-    kakao: any;
-  }
-
-  interface latlng {
-    latitude: number;
-    longitude: number;
-  }
+interface latlng {
+  latitude: number;
+  longitude: number;
 }
 
 const Map: React.FC = () => {
-  const mapWrapper = useRef<HTMLDivElement>(null);
-  const map = useRef(null);
+  const mapWrapper = useRef<HTMLDivElement | null>(null);
+  const [map, setMap] = useState<kakao.maps.Map | null>(null);
   const [{ latitude, longitude }, setLatlng] = useState({
     latitude: 33.450701,
     longitude: 126.570667,
   });
 
   // 초기 위치 값 가져오기
-  const getLocation = (map) => {
+  const getLocation = (map: kakao.maps.Map) => {
+    let coord: [number, number] = [37.5642135, 127.0016985];
+
+    const successCallback: PositionCallback = (position) => {
+      const { latitude, longitude } = position.coords;
+      coord = [latitude, longitude];
+      map.setCenter(new kakao.maps.LatLng(...coord));
+    };
+
+    const errorCallback: PositionErrorCallback = () => {
+      map.setCenter(new kakao.maps.LatLng(...coord));
+    };
+
     if (navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition((position) => {
-        const { latitude, longitude } = position.coords;
-        const coord = new window.kakao.maps.LatLng(latitude, longitude);
-        map.setCenter(coord);
-      });
+      navigator.geolocation.getCurrentPosition(successCallback, errorCallback);
     } else {
-      const coord = new window.kakao.maps.LatLng(33.450701, 126.570667);
-      map.setCenter(coord);
+      errorCallback(new GeolocationPositionError());
     }
   };
 
-  // 중심 Position 변경 시 동작하는 handler
-  const changeCenter = (map) => () => {
-    const latlng = map.getCenter();
-    setLatlng({ latitude: latlng.getLat(), longitude: latlng.getLng() });
-  };
-
   useEffect(() => {
+    if (!mapWrapper.current) {
+      return;
+    }
+
+    const wrapper = mapWrapper.current;
     const options = {
-      center: new window.kakao.maps.LatLng(latitude, longitude),
-      level: 3,
+      center: new kakao.maps.LatLng(33.450701, 126.570667),
+      level: 7,
     };
 
-    map.current = new window.kakao.maps.Map(mapWrapper.current, options);
-    getLocation(map.current);
+    const kakaoMap = new kakao.maps.Map(wrapper, options);
+    setMap(kakaoMap);
+    getLocation(kakaoMap);
 
-    window.kakao.maps.event.addListener(
-      map.current,
-      'dragend',
-      changeCenter(map.current),
-    );
+    const changeCenter = () => {
+      const latlng = kakaoMap.getCenter();
+      setLatlng({ latitude: latlng.getLat(), longitude: latlng.getLng() });
+    };
+
+    kakao.maps.event.addListener(kakaoMap, 'dragend', changeCenter);
     return () =>
-      window.kakao.maps.event.removeListener(
-        map.current,
-        'dragend',
-        changeCenter(map.current),
-      );
+      kakao.maps.event.removeListener(kakaoMap, 'dragend', changeCenter);
   }, []);
+
+  useEffect(() => {
+    if (!map) return;
+
+    (async () => {
+      const originCoords = await transCoord();
+
+      const convertGeoJson = (origin) =>
+        origin.map((region) => ({
+          name: region.name,
+          path: region.path.map(
+            (coord: [number, number]) => new kakao.maps.LatLng(...coord),
+          ),
+        }));
+
+      const displayArea = (area) => {
+        console.log('ArEA!!', area);
+
+        return new kakao.maps.Polygon({
+          map: map,
+          path: area.path,
+          strokeWeight: 2,
+          strokeColor: '#004c80',
+          strokeOpacity: 0.8,
+          fillColor: '#fff',
+          fillOpacity: 0.7,
+        });
+      };
+
+      const convertedAreas = await convertGeoJson(originCoords);
+      console.log(convertedAreas);
+      convertedAreas.forEach(displayArea);
+    })();
+  }, [map]);
 
   return <MapWrapper ref={mapWrapper} />;
 };
