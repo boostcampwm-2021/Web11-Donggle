c1124d779f4e08c3e97e7161015bc9fa0aa6f25c
hyehyeonmoon <angelfox4@naver.com>
2021-11-14 00:53:54 +0900

    Feat: #84 - 장소검색 바 삽입
    검프로필 페이지 장소설정 모달과 똑같기 때문에 이후 수정할 예정

--- client/src/components/SignInPlate/index.tsx
+++ client/src/components/SignInPlate/index.tsx
@@ -1,11 +1,20 @@
 import React from 'react';
 
+import SearchBar from '@components/Searchbar/signIn';
 import Modal from '@components/modal';
+import { SignInTitle, ButtonWrapper, SubmitButton } from './index.style';
 
 const SignInPlate: React.FC = () => {
   return (
     <Modal>
-      <div>회원가입창</div>
+      <SignInTitle>
+        회원가입을 위해서 우리 동네 정보를 입력해주세요!
+      </SignInTitle>
+      <SearchBar />
+      <ButtonWrapper>
+        <SubmitButton cancel={false}>제출</SubmitButton>
+        <SubmitButton cancel={true}>취소</SubmitButton>
+      </ButtonWrapper>
     </Modal>
   );
 };
