cc0b10a6aa1a5f5227ebe83571689f00656ccf16
isanghaessi <jesse1231@naver.com>
2021-11-14 21:02:09 +0900

    Chore: #69 - 테스트 코드 원상 복구

--- client/src/stores/atoms.ts
+++ client/src/stores/atoms.ts
@@ -3,9 +3,9 @@ import { atom } from 'recoil';
 const authState = atom({
   key: 'authentication',
   default: {
-    isLoggedin: true,
-    oauth_email: 'github-isanghaessi',
-    address: '인천광역시 부평구 부평4동',
+    isLoggedin: false,
+    oauth_email: '',
+    address: '',
     image: '',
   },
 });
