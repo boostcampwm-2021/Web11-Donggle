76de574e9036331c21daa7b63a212cf95f36d6cc
hyehyeonmoon <angelfox4@naver.com>
2021-11-22 17:09:41 +0900

    Feat: #139 - refreshToken 모델 schema 수정
    refreshToken이 들어갈 수 있도록 db User schema 수정

--- server/src/models/User.ts
+++ server/src/models/User.ts
@@ -8,14 +8,16 @@ interface User {
   code: string;
   center: CoordType;
   image?: string;
+  refreshToken?: string;
 }
 
 const userSchema = new Schema<User>({
   oauth_email: { type: String, index: true },
-  address: { type: String },
+  address: { type: String, required: true },
   code: { type: String },
   center: { type: [Number, Number] },
   image: { type: String },
+  refreshToken: { type: String, required: true },
 });
 
 const UserModel = model<User>('User', userSchema);
