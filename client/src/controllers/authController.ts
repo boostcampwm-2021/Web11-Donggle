4550e1dcc11a51c9257b456725825a0100e07f5b
hyehyeonmoon <angelfox4@naver.com>
2021-11-22 09:24:08 +0900

    Feat: #126 - refresh token 프론트엔드 로직 구현
    프론트엔드에서 토큰 만료를 체크하고 만료되었다면 재발급을 백엔드로 요청
    응답에 따르 에러처리

--- client/src/controllers/authController.ts
+++ client/src/controllers/authController.ts
@@ -1,15 +1,25 @@
 import { isJwtExpired } from 'jwt-check-expiration';
+
+import { IAPIResult } from '@myTypes/Common';
 import { IToken } from '@myTypes/User';
 
-// 로그인이 이미 되어 있어서 sessionstorage에 토큰이 있다고 가정함
-const checkExpired = async () => {
+/*
+2021-11-20
+문혜현
+token이 잘못되었거나 access와 refresh token이 만료된 경우로 모든 경우에 대해서 sessionStorage를 비우고 로그인 페이지로 이동시킴
+*/
+const checkExpired = async (routeHistory) => {
   const token = sessionStorage.getItem('jwt');
+
   if (!isJwtExpired(token)) {
     return;
   }
   const requestHeaders: HeadersInit = new Headers();
   requestHeaders.set('token', sessionStorage.getItem('jwt') as string);
-  requestHeaders.set('token', sessionStorage.getItem('refreshToken') as string);
+  requestHeaders.set(
+    'refreshToken',
+    sessionStorage.getItem('refreshToken') as string,
+  );
 
   const newTokenRes = await fetch(
     `${process.env.REACT_APP_API_URL as string}/api/auth/refresh`,
@@ -19,7 +29,21 @@ const checkExpired = async () => {
     },
   );
 
-  const newToken: IToken = await newTokenRes.json();
+  const newToken: IAPIResult<IToken | Record<string, never>> =
+    await newTokenRes.json();
 
-  sessionStorage.setItem('jwt', newToken.jwtToken);
+  if (newTokenRes.status !== 200) {
+    alert(newToken.message);
+    /*
+    2021-11-20
+    문혜현
+    token이 잘못되었거나 access와 refresh token이 만료된 경우로 모든 경우에 대해서 sessionStorage를 비우고 로그인 페이지로 이동시킴
+    */
+    sessionStorage.clear();
+    routeHistory('/signin', { background: { pathname: '/' } });
+  } else {
+    sessionStorage.setItem('jwt', newToken.result.jwtToken);
+  }
 };
+
+export { checkExpired };
