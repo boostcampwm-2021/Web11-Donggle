bf5bb66300ffcd49797a7fceb356ec535aff55bb
isanghaessi <jesse1231@naver.com>
2021-11-22 21:32:49 +0900

    Fix: #147 - 플리곤 색상 겹치지 않도록 수정
    - 폴리곤이 색상이 겹치면(많이 비슷하면) 새로운 랜덤 숫자를 만들어냄

--- client/src/controllers/mapController.ts
+++ client/src/controllers/mapController.ts
@@ -104,19 +104,29 @@ const requestCoord = async (
     });
 };
 
+const isLike = (color1: string, color2: string) => {
+  color1 = color1.replace('#', '');
+  color2 = color2.replace('#', '');
+  return (
+    Array.from(color1).filter((_, i) => color1[i] === color2[i]).length >= 3
+  );
+};
+
 const createPolygons = (regions) => {
   const polygons = Array<IPolygon>();
   const colorHash = new ColorHash();
+  const colors = [''];
   regions.forEach((region) => {
-    const colorString = colorHash.hex(region.address);
+    let color: string = colorHash.hex(region.address);
+    let i = 0;
+    while (colors.filter((c) => isLike(c, color)).length) {
+      color = colorHash.hex(`${region.address}${i++}`);
+    }
+    colors.push(color);
     if (region.type === 'Polygon') {
-      polygons.push(
-        makeSinglePolygon(region.path, region.address, colorString),
-      );
+      polygons.push(makeSinglePolygon(region.path, region.address, color));
     } else {
-      polygons.push(
-        ...makeMultiPolygon(region.path, region.address, colorString),
-      );
+      polygons.push(...makeMultiPolygon(region.path, region.address, color));
     }
   });
   return polygons;
