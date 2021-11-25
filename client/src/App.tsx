8158df6de53eae85c3fc38e81e3788d81e1981e6
J219_홍종우 <hongjw1938@gmail.com>
2021-11-25 13:35:45 +0900

    [Refactor] Lazy Loading 설정을 통한 초기 렌더링 최적화 (#163)
    * Fix: eslint disable 예외 처리 문구 제거
    
    * Refactor: App.tsx Browser Router 내 각 Page Lazy Loading 설정
    
    * Fix: Loading Page는 정적 로드되도록 설정, Route는 render로 공통 사용

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,16 +1,3 @@
-/* eslint-disable react/no-children-prop */
-import {
-  NotFoundPage,
-  MainPage,
-  ReviewSubmitPage,
-  LoadPage,
-  RankingPage,
-  SignInPage,
-  LoadingPage,
-  SignUpPage,
-  ProfilePage,
-  ProfileAddressPage,
-} from '@pages/index';
 import { GlobalStore } from '@stores/index';
 import GlobalStyle from '@styledComponents/GlobalStyle';
 import myTheme from '@styledComponents/theme';
@@ -18,9 +5,22 @@ import Header from '@components/Header/index';
 import Snackbar from '@components/Snackbar';
 import PrivateRoute from '@routes/PrivateRoute';
 
-import React from 'react';
+import React, { lazy, Suspense } from 'react';
 import { Switch, Route, Redirect } from 'react-router-dom';
 import styled, { ThemeProvider } from 'styled-components';
+
+import LoadAnimation from '@components/Loading';
+import { LoadingPage } from '@pages/index';
+
+const MainPage = lazy(() => import('@pages/MainPage'));
+const LoadPage = lazy(() => import('@pages/LoadPage'));
+const RankingPage = lazy(() => import('@pages/RankingPage'));
+const SignInPage = lazy(() => import('@pages/SignInPage'));
+const SignUpPage = lazy(() => import('@pages/SignUpPage'));
+const ProfilePage = lazy(() => import('@pages/ProfilePage'));
+const ProfileAddressPage = lazy(() => import('@pages/ProfileAddressPage'));
+const ReviewSubmitPage = lazy(() => import('@pages/ReviewSubmitPage'));
+const NotFoundPage = lazy(() => import('@pages/NotFoundPage'));
 
 const ContentWrapper = styled.div`
   width: 100%;
@@ -37,31 +37,33 @@ const App: React.FC = () => {
       <ThemeProvider theme={myTheme}>
         <GlobalStore>
           <ContentWrapper>
-            <Snackbar />
-            <Header />
-            <Switch>
-              <Route
-                exact
-                path="/"
-                component={() => <Redirect to={{ pathname: '/map' }} />}
+            <Suspense fallback={<LoadAnimation />}>
+              <Snackbar />
+              <Header />
+              <Switch>
+                <Route
+                  exact
+                  path="/"
+                  component={() => <Redirect to={{ pathname: '/map' }} />}
+                />
+                <Route path="/map" render={() => <MainPage />} />
+                <Route path="/github/callback" render={() => <LoadingPage />} />
+                <Route path="/loading" render={() => <LoadPage />} />
+                <PrivateRoute path="/profile" component={ProfilePage} />
+                <Route render={() => <NotFoundPage />} />
+              </Switch>
+              <PrivateRoute
+                path="/map/write-review"
+                component={ReviewSubmitPage}
               />
-              <Route path="/map" render={() => <MainPage />} />
-              <Route path="/github/callback" render={() => <LoadingPage />} />
-              <Route path="/loading" render={() => <LoadPage />} />
-              <PrivateRoute path="/profile" component={ProfilePage} />
-              <Route render={() => <NotFoundPage />} />
-            </Switch>
-            <PrivateRoute
-              path="/map/write-review"
-              component={ReviewSubmitPage}
-            />
-            <Route path="/map/ranking" render={() => <RankingPage />} />
-            <Route path="/map/signin" component={SignInPage} />
-            <Route path="/map/signup" render={() => <SignUpPage />} />
-            <PrivateRoute
-              path="/profile/update-address"
-              component={ProfileAddressPage}
-            />
+              <Route path="/map/ranking" render={() => <RankingPage />} />
+              <Route path="/map/signin" render={() => <SignInPage />} />
+              <Route path="/map/signup" render={() => <SignUpPage />} />
+              <PrivateRoute
+                path="/profile/update-address"
+                component={ProfileAddressPage}
+              />
+            </Suspense>
           </ContentWrapper>
         </GlobalStore>
       </ThemeProvider>
