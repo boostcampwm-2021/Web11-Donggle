5376f4411c1641f0d11227bf5c70b601ed867140
Myeonghoe Song <tintin5108@gmail.com>
2021-11-26 01:36:17 +0900

    feat: #161 - Profile 재렌더링 최소화
    - props로 넘겨지는 익명함수들을 useCallback을 활용하여 메모
    - React.memo를 활용하여 상호작용별 불필요한 재렌더링 최소화
    - 모달이 띄워지는 상황(주소 업데이트)에서 배경 컴포넌트 재렌더링 안되도록 개선

--- client/src/pages/ProfileAddressPage.tsx
+++ client/src/pages/ProfileAddressPage.tsx
@@ -4,9 +4,10 @@ import { authState } from '@stores/atoms';
 import { useRecoilState } from 'recoil';
 import useHistoryRouter from '@hooks/useHistoryRouter';
 
-import React from 'react';
+import React, { useCallback, useMemo } from 'react';
 import styled from 'styled-components';
 import { useLocation } from 'react-router-dom';
+import { getPrevPath } from '@utils/common';
 
 const AddressWrapper = styled.div`
   width: 100%;
@@ -15,16 +16,25 @@ const AddressWrapper = styled.div`
 
 const ProfileAddressPage: React.FC = () => {
   const routeHistory = useHistoryRouter();
-  const location = useLocation();
-
+  const { pathname } = useLocation();
   const [auth, setAuth] = useRecoilState(authState);
+
+  const onSubmitHandler = useMemo(
+    () => updateAddress(auth, setAuth),
+    [auth, setAuth],
+  );
+
+  const onCancelHandler = useCallback(
+    () => routeHistory(getPrevPath(pathname)),
+    [pathname, routeHistory],
+  );
 
   return (
     <AddressWrapper>
       <AddressModal
         title="우리 동네를 입력해주세요!"
-        onSubmitHandler={updateAddress(auth, setAuth)}
-        onCancelHandler={() => routeHistory(location.pathname, {})}
+        onSubmitHandler={onSubmitHandler}
+        onCancelHandler={onCancelHandler}
       />
     </AddressWrapper>
   );
