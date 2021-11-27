3ae2e6d1a9772f668a49cd97038dbf112a65dba1
J219_홍종우 <hongjw1938@gmail.com>
2021-11-27 10:56:18 +0900

    Refactor: for-of 문 Promise.all 리팩토링하여 병렬 처리 (#171)

--- server/src/services/reviewService.ts
+++ server/src/services/reviewService.ts
@@ -148,10 +148,11 @@ const updateMapInfoHashtag = async (
       { codeLength: 5, code: code.slice(0, 5) },
       { codeLength: 7, code: code },
     ];
+    const promises = conditions.map((condition) =>
+      findAndModifyHashtag(condition, hashtags, session),
+    );
 
-    for (const condition of conditions) {
-      await findAndModifyHashtag(condition, hashtags, session);
-    }
+    await Promise.all(promises);
   }
 };
 
