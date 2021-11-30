ef2cabf5fd8843212e93c0031f37983e8ddf2764
hyehyeonmoon <angelfox4@naver.com>
2021-11-30 17:53:12 +0900

    Fix: #198 - 새로고침, 자동로그인, 로그인하지 않을 시 막는 privateRoute 고침
    recoil로 로그인 여부를 확인하고 직접 치거나 새로고침 시 쿠키가 살아있으므로 백엔드에 로그인 유효성 검사

--- client/src/routes/PrivateRoute.tsx
+++ client/src/routes/PrivateRoute.tsx
@@ -5,53 +5,22 @@ import { authState } from '@stores/atoms';
 
 const PrivateRoute: React.FC<RouterProps> = ({
   component: Component,
-  needSignIn: needSignIn,
   ...rest
 }) => {
   const auth = useRecoilValue(authState);
   const location = useLocation();
 
   const checkAll = () => {
-    try {
-      const expireTime = sessionStorage.getItem('timer');
-
-      if (!expireTime) {
-        //로그인을 안 한 상태
-        if (needSignIn) {
-          return <Redirect to={{ pathname: '/map/signin' }} />;
-        } else {
-          return <Component />;
-        }
-      }
-      if (!auth.isLoggedin) {
-        //로그인은 했지만 새로고침
-        return (
-          <Redirect
-            to={{ pathname: '/loading', state: { isRoute: true, ...location } }}
-          />
-        );
-      }
-
-      const now = new Date();
-      const currentTime = now.getTime();
-      if (
-        currentTime - Number(expireTime) >
-        Number(process.env.REACT_APP_TIMER)
-      ) {
-        //로그인한 상태인데 token이 만료
-        return (
-          <Redirect
-            to={{ pathname: '/loading', state: { isRoute: true, ...location } }}
-          />
-        );
-      } else {
-        //로그인한 상태이며 token이 유효한 상태
-        return <Component />;
-      }
-    } catch (error) {
-      alert(error);
-      return <Redirect to={{ pathname: '/map/signin' }} />;
+    if (!auth.isLoggedin) {
+      //로그인을 하지 않은 상태 또는 로그인은 했지만 새로고침
+      return (
+        <Redirect
+          to={{ pathname: '/loading', state: { isRoute: true, ...location } }}
+        />
+      );
     }
+    //로그인한 상태
+    return <Component />;
   };
 
   return <Route {...rest} render={checkAll} />;
