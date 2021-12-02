c6dcbecfd5a04302b225141ce3334a7f1463ed57
Myeonghoe Song <tintin5108@gmail.com>
2021-12-02 09:09:28 +0900

    fix: #206 - 마커 위에서 드래그했을 때 사이드바가 열리는 이슈
    - 맵 드래그가 끝났을 때 다음 이벤트 사이클까지 true로 남아있는 `isDragged` 추가
    - kakao.maps.Map에 `dragend` 이벤트 핸들러 추가
    - `isDragged === true`이면 마커 클릭 이벤트 핸들러가 실행되지 않도록 수정

--- client/src/components/Map/index.tsx
+++ client/src/components/Map/index.tsx
@@ -59,6 +59,7 @@ const MapComponent: React.FC<IProps> = ({
   updateSidebarContents,
 }) => {
   const mapWrapper = useRef<HTMLDivElement | null>(null);
+  const isDragged = useRef(false);
   const [map, setMap] = useState<kakao.maps.Map | null>(null);
   const [range, setRange] = useState(DEFAULT_RANGE);
 
@@ -140,17 +141,27 @@ const MapComponent: React.FC<IProps> = ({
       }
     };
 
+    const notifyDragend = () => {
+      isDragged.current = true;
+      setTimeout(() => {
+        isDragged.current = false;
+      });
+    };
+
+    kakao.maps.event.addListener(kakaoMap, 'dragend', notifyDragend);
     kakao.maps.event.addListener(kakaoMap, 'idle', updateRange);
     return () => {
+      kakao.maps.event.removeListener(kakaoMap, 'dragend', notifyDragend);
       kakao.maps.event.removeListener(kakaoMap, 'idle', updateRange);
     };
   }, []);
 
   useEffect(() => {
     if (!mapWrapper.current) return;
-
     const wrapper = mapWrapper.current;
+
     const onClick = async (rateData: IMapInfo) => {
+      if (isDragged.current) return;
       const sidebarContents: IAPIResult<IReviewContent[]> =
         await fetchContentData(rateData.address, 'review');
 
@@ -158,8 +169,13 @@ const MapComponent: React.FC<IProps> = ({
       updateSidebarContents(sidebarContents.result || []);
       openSidebar();
     };
-    const onMarkerClicked = createMarkerClickListener(onClick, closeSidebar);
 
+    const onOutsideClick = () => {
+      if (isDragged.current) return;
+      closeSidebar();
+    };
+
+    const onMarkerClicked = createMarkerClickListener(onClick, onOutsideClick);
     wrapper.addEventListener('click', onMarkerClicked);
     return () => wrapper.removeEventListener('click', onMarkerClicked);
   }, [openSidebar, closeSidebar, updateSidebarRate, updateSidebarContents]);
